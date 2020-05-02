5ceb9691cd166abba2d5deab6991bd2d0eb7cef8
Lyman Johnson <lyman.johnson@gmail.com>
2020-05-02 03:22:37 -0400

    Adds a new version of the core lua to fashion montage 2 to deal with the new changes

---
+++ FashionMontage/Contents/mods/FashionMontage2_Colors/media/lua/client/Definitions/OGSNfashionMontage.lua
@@ -0,0 +1,488 @@
+-- Lists of all items --
+
+local listHat = {
+  "Base.Hat_Bandana",
+  "Base.Hat_BandanaTied",
+  "Base.Hat_BandanaTINT",
+  "Base.Hat_BandanaTiedTINT",
+  "Base.Hat_BaseballCap",
+  "Base.Hat_BaseballCap_Reverse",
+  "Base.Hat_BaseballCapBlue",
+  "Base.Hat_BaseballCapBlue_Reverse",
+  "Base.Hat_BaseballCapRed",
+  "Base.Hat_BaseballCapRed_Reverse",
+  "Base.Hat_BaseballCapGreen",
+  "Base.Hat_BaseballCapGreen_Reverse",
+  "Base.Hat_BaseballCapKY",
+  "Base.Hat_BaseballCapKY_Reverse",
+  "Base.Hat_BaseballCapKY_Red",
+  "Base.Hat_Beany",
+  "Base.Hat_Beret",
+  "Base.Hat_BeretArmy",
+  "Base.Hat_BonnieHat_CamoGreen",
+  -- "Base.Hat_BonnieHat",
+  "Base.Hat_ChefHat",
+  "Base.Hat_Cowboy",
+  "Base.Hat_EarMuffs",
+  "Base.Hat_EarMuff_Protectors",
+  "Base.Hat_Fedora",
+  -- "Base.Hat_Fedora_Delmonte",
+  "Base.Hat_Ranger",
+  "Base.Hat_Police_Grey",
+  "Base.Hat_Police",
+  "Base.Hat_GolfHatTINT",
+  "Base.Hat_GolfHat",
+  "Base.Hat_HardHat",
+  -- "Base.Hat_HardHat_Miner",
+  "Base.Hat_Army",
+  "Base.Hat_BaseballHelmet_KY",
+  "Base.Hat_BaseballHelmet_Rangers",
+  "Base.Hat_BaseballHelmet_Z",
+  "Base.Hat_BicycleHelmet",
+  "Base.Hat_CrashHelmet",
+  "Base.Hat_CrashHelmet_Police",
+  "Base.Hat_CrashHelmet_Stars",
+  "Base.Hat_Fireman",
+  "Base.Hat_HockeyHelmet",
+  "Base.Hat_JockeyHelmet",
+  -- "Base.Hat_JockeyHelmet01",
+  -- "Base.Hat_JockeyHelmet02",
+  -- "Base.Hat_JockeyHelmet03",
+  -- "Base.Hat_JockeyHelmet04",
+  -- "Base.Hat_JockeyHelmet05",
+  -- "Base.Hat_JockeyHelmet06",
+  "Base.Hat_RidingHelmet",
+  "Base.Hat_PeakedCapArmy",
+  "Base.Hat_Raccoon",
+  "Base.Hat_FastFood",
+  "Base.Hat_FastFood_IceCream",
+  "Base.Hat_FastFood_Spiffo",
+  "Base.Hat_SantaHat",
+  "Base.Hat_SantaHatGreen",
+  "Base.Hat_ShowerCap",
+  "Base.Hat_SummerHat",
+  -- "Base.Hat_SurgicalCap_Blue",
+  "Base.Hat_SurgicalCap_Green",
+  "Base.Hat_Sweatband",
+  "Base.Hat_WeddingVeil",
+  "Base.Hat_WinterHat",
+  "Base.Hat_WoolyHat",
+  "Base.Hat_Visor_WhiteTINT",
+  -- "Base.Hat_VisorBlack",
+  -- "Base.Hat_VisorRed",
+}
+local listTankTop = {
+  "Base.Vest_DefaultTEXTURE_TINT",
+}
+local listTshirt = {
+  -- "Base.Tshirt_DefaultTEXTURE",
+  "Base.Tshirt_DefaultTEXTURE_TINT",
+  "Base.Tshirt_WhiteTINT",
+  -- "Base.Tshirt_WhiteLongSleeve",
+  "Base.Tshirt_WhiteLongSleeveTINT",
+  "Base.Tshirt_PoloStripedTINT",
+  "Base.Tshirt_PoloTINT",
+  -- "Base.Tshirt_DefaultDECAL",
+  "Base.Tshirt_DefaultDECAL_TINT",
+  -- "Base.Tshirt_SportDECAL",
+  "Base.Tshirt_Rock",
+  "Base.Tshirt_Sport",
+  "Base.Tshirt_ArmyGreen",
+  "Base.Tshirt_CamoDesert",
+  "Base.Tshirt_CamoGreen",
+  "Base.Tshirt_CamoUrban",
+  "Base.Tshirt_Fossoil",
+  "Base.Tshirt_Gas2Go",
+  "Base.Tshirt_McCoys",
+  "Base.Tshirt_PileOCrepe",
+  "Base.Tshirt_PizzaWhirled",
+  "Base.Tshirt_BusinessSpiffo",
+  "Base.Tshirt_SpiffoDECAL",
+  "Base.Tshirt_ThunderGas",
+  "Base.Tshirt_ValleyStation",
+  "Base.Tshirt_IndieStoneDECAL",
+  "Base.Tshirt_Profession_FiremanBlue",
+  "Base.Tshirt_Profession_FiremanRed02",
+  "Base.Tshirt_Profession_FiremanRed",
+  "Base.Tshirt_Profession_FiremanWhite",
+  "Base.Tshirt_Scrubs",
+  "Base.Tshirt_Ranger",
+  "Base.Tshirt_Profession_RangerBrown",
+  "Base.Tshirt_Profession_RangerGreen",
+  "Base.Tshirt_Profession_PoliceBlue",
+  "Base.Tshirt_Profession_PoliceWhite",
+  "Base.Tshirt_PoliceGrey",
+  "Base.Tshirt_PoliceBlue",
+  "Base.Tshirt_Profession_VeterenGreen",
+  "Base.Tshirt_Profession_VeterenRed",
+}
+local listShirt = {
+  "Base.Shirt_FormalWhite",
+  "Base.Shirt_Denim",
+  "Base.Shirt_FormalWhite_ShortSleeve",
+  "Base.Shirt_CamoDesert",
+  "Base.Shirt_CamoGreen",
+  "Base.Shirt_CamoUrban",
+  "Base.Shirt_Baseball_KY",
+  "Base.Shirt_Baseball_Rangers",
+  "Base.Shirt_Baseball_Z",
+  "Base.Shirt_HawaiianTINT",
+  "Base.Shirt_HawaiianRed",
+  "Base.Shirt_Jockey01",
+  "Base.Shirt_Jockey02",
+  "Base.Shirt_Jockey03",
+  "Base.Shirt_Jockey04",
+  "Base.Shirt_Jockey05",
+  "Base.Shirt_Jockey06",
+  "Base.Shirt_Lumberjack",
+  "Base.Shirt_Scrubs",
+  "Base.Shirt_Ranger",
+  "Base.Shirt_OfficerWhite",
+  "Base.Shirt_PoliceBlue",
+  "Base.Shirt_PoliceGrey",
+  "Base.Shirt_PrisonGuard",
+  "Base.Shirt_Priest",
+  "Base.Shirt_Workman",
+}
+local listSocks = {
+  "Base.Socks_Ankle",
+  "Base.Socks_Long",
+}
+local listPants = {
+  "Base.Shorts_CamoGreenLong",
+  "Base.Shorts_LongDenim",
+  "Base.Shorts_LongSport",
+  -- "Base.Shorts_LongSport_Red",
+  "Base.Shorts_ShortDenim",
+  "Base.Shorts_ShortFormal",
+  "Base.Shorts_ShortSport",
+  "Base.Ghillie_Trousers",
+  -- "Base.Trousers",
+  "Base.TrousersMesh_DenimLight",
+  "Base.Trousers_CamoDesert",
+  "Base.Trousers_CamoGreen",
+  "Base.Trousers_CamoUrban",
+  "Base.Trousers_Chef",
+  -- "Base.Trousers_DefaultTEXTURE",
+  -- "Base.Trousers_DefaultTEXTURE_HUE",
+  "Base.Trousers_DefaultTEXTURE_TINT",
+  "Base.Trousers_Denim",
+  "Base.Trousers_Fireman",
+  "Base.Trousers_JeanBaggy",
+  "Base.Trousers_Padded",
+  "Base.Trousers_Police",
+  "Base.Trousers_PoliceGrey",
+  "Base.Trousers_PrisonGuard",
+  "Base.Trousers_Ranger",
+  "Base.Trousers_Santa",
+  "Base.Trousers_SantaGReen",
+  "Base.Trousers_Scrubs",
+  "Base.Trousers_Suit",
+  -- "Base.Trousers_SuitTEXTURE",
+  "Base.Trousers_SuitWhite",
+  -- "Base.Trousers_WhiteTEXTURE",
+  -- "Base.Trousers_WhiteTINT",
+  "Base.Dungarees",
+  "Base.Trousers_ArmyService",
+  "Base.Trousers_Black",
+  "Base.Trousers_NavyBlue",
+}
+local listSkirt = {
+  "Base.Skirt_Long",
+  "Base.Skirt_Normal",
+  "Base.Skirt_Knees",
+  "Base.Skirt_Short",
+}
+local listDress = {
+  "Base.Dress_Long",
+  "Base.Dress_Normal",
+  "Base.Dress_Knees",
+  "Base.Dress_Short",
+  "Base.HospitalGown",
+}
+local listShoes = {
+  -- "Base.Shoes_Black",
+  -- "Base.Shoes_Brown",
+  "Base.Shoes_Random",
+  -- "Base.Shoes_RedTrainers",
+  -- "Base.Shoes_BlueTrainers",
+  "Base.Shoes_TrainerTINT",
+  "Base.Shoes_BlackBoots",
+  "Base.Shoes_ArmyBoots",
+  "Base.Shoes_ArmyBootsDesert",
+  "Base.Shoes_Wellies",
+  "Base.Shoes_RidingBoots",
+  "Base.Shoes_Slippers",
+}
+local listEyes = {
+  "Base.Glasses_Normal",
+  "Base.Glasses_Reading",
+  "Base.Glasses_SafetyGoggles",
+  "Base.Glasses_Shooting",
+  "Base.Glasses_SkiGoggles",
+  "Base.Glasses_SwimmingGoggles",
+  "Base.Glasses_Sun",
+  "Base.Glasses_Aviators",
+  "Base.Glasses",
+}
+local listBeltExtra = {
+  "Base.HolsterSimple",
+  "Base.HolsterDouble",
+}
+local listMask = {
+  "Base.Hat_BalaclavaFull",
+  "Base.Hat_BalaclavaFace",
+  "Base.Hat_BandanaMask",
+  "Base.Hat_BandanaMaskTINT",
+  "Base.Hat_DustMask",
+  -- "Base.Hat_SurgicalMask_Blue",
+  "Base.Hat_SurgicalMask_Green",
+}
+local listMaskEyes = {
+  "Base.Hat_GasMask",
+  "Base.WeldingMask",
+}
+local listUnderwear = {
+  "Base.Bikini_TINT",
+  "Base.Bikini_Pattern01",
+  "Base.SwimTrunks_Blue",
+  "Base.SwimTrunks_Green",
+  "Base.SwimTrunks_Red",
+  "Base.SwimTrunks_Yellow",
+  "Base.Swimsuit_TINT",
+}
+local listFullHat = {
+  "Base.Hat_SPHhelmet",
+  "Base.Hat_FootballHelmet",
+  "Base.Hat_CrashHelmetFULL",
+  "Base.Hat_RiotHelmet",
+  "Base.Hat_NBCmask",
+  "Base.Hat_Spiffo",
+}
+local listTorso1Legs1 = {
+  "Base.LongJohns",
+}
+local listNeck = {
+  "Base.Tie_Full",
+  "Base.Tie_Worn",
+  "Base.Tie_BowTieFull",
+  "Base.Tie_BowTieWorn",
+  "Base.Tie_Full_Spiffo",
+  "Base.Tie_Worn_Spiffo",
+}
+local listHands = {
+  "Base.Gloves_FingerlessGloves",
+  "Base.Gloves_LeatherGloves",
+  "Base.Gloves_LeatherGlovesBlack",
+  "Base.Gloves_LongWomenGloves",
+  "Base.Gloves_WhiteTINT",
+}
+local listLegs1 = {
+  "Base.LongJohns_Bottoms"
+}
+local listSweater = {
+  "Base.Jumper_DiamondPatternTINT",
+  "Base.Jumper_TankTopDiamondTINT",
+  "Base.HoodieDOWN_WhiteTINT",
+  "Base.Jumper_PoloNeck",
+  "Base.Jumper_RoundNeck",
+  "Base.Jumper_VNeck",
+  "Base.Jumper_TankTopTINT",
+}
+local listSweaterHat = {
+  "Base.HoodieUP_WhiteTINT",
+}
+local listJacket = {
+  "Base.Jacket_WhiteTINT",
+  "Base.Jacket_Black",
+  "Base.JacketLong_Random",
+  "Base.Jacket_Chef",
+  "Base.Jacket_Fireman",
+  "Base.JacketLong_Doctor",
+  "Base.Jacket_ArmyCamoDesert",
+  "Base.Jacket_ArmyCamoGreen",
+  "Base.Jacket_CoatArmy",
+  -- "Base.Jacket_NavyBlue",
+  "Base.Jacket_PaddedDOWN",
+  "Base.Jacket_Ranger",
+  "Base.Jacket_Police",
+  -- "Base.PonchoGreenDOWN",
+  "Base.PonchoYellowDOWN",
+  "Base.JacketLong_Santa",
+  "Base.JacketLong_SantaGreen",
+  "Base.Suit_Jacket",
+  "Base.WeddingJacket",
+  "Base.Jacket_Varsity",
+}
+local listJacketHat = {
+  "Base.Jacket_Padded",
+  -- "Base.PonchoGreen",
+  "Base.PonchoYellow",
+}
+local listFullSuit = {
+  "Base.Boilersuit",
+  "Base.Boilersuit_BlueRed",
+  "Base.Boilersuit_Flying",
+  "Base.Boilersuit_PrisonerKhaki",
+  "Base.Boilersuit_Prisoner",
+  "Base.SpiffoSuit",
+  "Base.WeddingDress",
+}
+local listFullSuitHead = {
+  "Base.HazmatSuit",
+}
+local listFullTop = {
+  "Base.Ghillie_Top",
+}
+local listBathRobe = {
+  "Base.LongCoat_Bathrobe",
+}
+local listTorsoExtra = {
+  "Base.Apron_Black",
+  "Base.Apron_White",
+  "Base.Apron_WhiteTEXTURE",
+  "Base.Apron_IceCream",
+  "Base.Apron_PileOCrepe",
+  "Base.Apron_PizzaWhirled",
+  "Base.Apron_Spiffos",
+  "Base.Vest_BulletCivilian",
+  "Base.Vest_BulletPolice",
+  "Base.Vest_BulletArmy",
+  "Base.Vest_Waistcoat",
+  "Base.Vest_Waistcoat_GigaMart",
+  "Base.Vest_Foreman",
+  "Base.Vest_HighViz",
+  "Base.Vest_Hunting_Grey",
+  "Base.Vest_Hunting_Orange",
+  "Base.Vest_Hunting_Camo",
+  "Base.Vest_Hunting_CamoGreen",
+}
+local listTail = {
+  "Base.SpiffoTail",
+}
+local listBack = {
+  "Base.Bag_ALICEpack_Army",
+  "Base.Bag_ALICEpack",
+  "Base.Bag_BigHikingBag",
+  "Base.Bag_NormalHikingBag",
+  "Base.Bag_DuffelBagTINT",
+  "Base.Bag_Schoolbag",
+}
+local listScarf = {
+  "Base.Scarf_White",
+  "Base.Scarf_StripeBlackWhite",
+  "Base.Scarf_StripeBlueWhite",
+  "Base.Scarf_StripeRedWhite",
+}
+
+ClothingSelectionDefinitions.default.Male.Hat = {chance = 10, items = listHat}
+ClothingSelectionDefinitions.default.Male.TankTop = {chance = 10, items = listTankTop}
+ClothingSelectionDefinitions.default.Male.Tshirt = {items = listTshirt}
+ClothingSelectionDefinitions.default.Male.Shirt = {chance = 10, items = listShirt}
+ClothingSelectionDefinitions.default.Male.Socks = {chance = 0, items = listSocks}
+ClothingSelectionDefinitions.default.Male.Pants = {items = listPants}
+ClothingSelectionDefinitions.default.Male.Skirt = {chance = 0, items = listSkirt}
+ClothingSelectionDefinitions.default.Male.Dress = {chance = 0, items = listDress}
+ClothingSelectionDefinitions.default.Male.Shoes = {items = listShoes}
+ClothingSelectionDefinitions.default.Male.Eyes = {chance = 10, items = listEyes}
+ClothingSelectionDefinitions.default.Male.BeltExtra = {chance = 0, items = listBeltExtra}
+ClothingSelectionDefinitions.default.Male.Mask = {chance = 0, items = listMask}
+ClothingSelectionDefinitions.default.Male.MaskEyes = {chance = 0, items = listMaskEyes}
+ClothingSelectionDefinitions.default.Male.Underwear = {chance = 0, items = listUnderwear}
+ClothingSelectionDefinitions.default.Male.FullHat = {chance = 0, items = listFullHat}
+ClothingSelectionDefinitions.default.Male.Torso1Legs1 = {chance = 0, items = listTorso1Legs1}
+ClothingSelectionDefinitions.default.Male.Neck = {chance = 0, items = listNeck}
+ClothingSelectionDefinitions.default.Male.Hands = {chance = 0, items = listHands}
+ClothingSelectionDefinitions.default.Male.Legs1 = {chance = 0, items = listLegs1}
+ClothingSelectionDefinitions.default.Male.Sweater = {chance = 30, items = listSweater}
+ClothingSelectionDefinitions.default.Male.Jacket = {chance = 30, items = listJacket}
+ClothingSelectionDefinitions.default.Male.FullSuit = {chance = 0, items = listFullSuit}
+ClothingSelectionDefinitions.default.Male.FullSuitHead = {chance = 0, items = listFullSuitHead}
+ClothingSelectionDefinitions.default.Male.FullTop = {chance = 0, items = listFullTop}
+ClothingSelectionDefinitions.default.Male.BathRobe = {chance = 0, items = listBathRobe}
+ClothingSelectionDefinitions.default.Male.TorsoExtra = {chance = 0, items = listTorsoExtra}
+ClothingSelectionDefinitions.default.Male.Tail = {chance = 0, items = listTail}
+ClothingSelectionDefinitions.default.Male.Back = {chance = 0, items = listBack}
+ClothingSelectionDefinitions.default.Male.Scarf = {chance = 0, items = listScarf}
+
+
+ClothingSelectionDefinitions.default.Female.Hat = {chance = 10, items = listHat}
+ClothingSelectionDefinitions.default.Female.TankTop = {chance = 10, items = listTankTop}
+ClothingSelectionDefinitions.default.Female.Tshirt = {items = listTshirt}
+ClothingSelectionDefinitions.default.Female.Shirt = {chance = 10, items = listShirt}
+ClothingSelectionDefinitions.default.Female.Socks = {chance = 0, items = listSocks}
+ClothingSelectionDefinitions.default.Female.Pants = {items = listPants}
+ClothingSelectionDefinitions.default.Female.Skirt = {chance = 0, items = listSkirt}
+ClothingSelectionDefinitions.default.Female.Dress = {chance = 0, items = listDress}
+ClothingSelectionDefinitions.default.Female.Shoes = {items = listShoes}
+ClothingSelectionDefinitions.default.Female.Eyes = {chance = 10, items = listEyes}
+ClothingSelectionDefinitions.default.Female.BeltExtra = {chance = 0, items = listBeltExtra}
+ClothingSelectionDefinitions.default.Female.Mask = {chance = 0, items = listMask}
+ClothingSelectionDefinitions.default.Female.MaskEyes = {chance = 0, items = listMaskEyes}
+ClothingSelectionDefinitions.default.Female.Underwear = {chance = 0, items = listUnderwear}
+ClothingSelectionDefinitions.default.Female.FullHat = {chance = 0, items = listFullHat}
+ClothingSelectionDefinitions.default.Female.Torso1Legs1 = {chance = 0, items = listTorso1Legs1}
+ClothingSelectionDefinitions.default.Female.Neck = {chance = 0, items = listNeck}
+ClothingSelectionDefinitions.default.Female.Hands = {chance = 0, items = listHands}
+ClothingSelectionDefinitions.default.Female.Legs1 = {chance = 0, items = listLegs1}
+ClothingSelectionDefinitions.default.Female.Sweater = {chance = 30, items = listSweater}
+ClothingSelectionDefinitions.default.Female.Jacket = {chance = 30, items = listJacket}
+ClothingSelectionDefinitions.default.Female.FullSuit = {chance = 0, items = listFullSuit}
+ClothingSelectionDefinitions.default.Female.FullSuitHead = {chance = 0, items = listFullSuitHead}
+ClothingSelectionDefinitions.default.Female.FullTop = {chance = 0, items = listFullTop}
+ClothingSelectionDefinitions.default.Female.BathRobe = {chance = 0, items = listBathRobe}
+ClothingSelectionDefinitions.default.Female.TorsoExtra = {chance = 0, items = listTorsoExtra}
+ClothingSelectionDefinitions.default.Female.Tail = {chance = 0, items = listTail}
+ClothingSelectionDefinitions.default.Female.Back = {chance = 0, items = listBack}
+ClothingSelectionDefinitions.default.Female.Scarf = {chance = 0, items = listScarf}
+
+
+--- MAKEUP ----
+
+-- local listMakeupLips = {
+--   "MakeUp_LipsRed",
+--   "MakeUp_LipsLightBlue",
+--   "MakeUp_LipsBlue",
+--   "MakeUp_LipsBlack",
+--   "MakeUp_LipsGreen",
+--   "MakeUp_LipsPink",
+-- }
+
+-- local listMakeupEyesShadow = {
+--   "MakeUp_EyesShadowWhite",
+--   "MakeUp_EyesShadowLightBlue",
+--   "MakeUp_EyesShadowBlue",
+--   "MakeUp_EyesShadowRed",
+--   "MakeUp_EyesShadowPink",
+--   "MakeUp_EyesShadowGreen",
+--   "MakeUp_EyesShadowYellow",
+-- }
+
+-- local listMakeupEyes = {
+--   "MakeUp_Crow",
+--   "MakeUp_RedStripes1",
+--   "MakeUp_RedStripes2",
+--   "MakeUp_CamoEyes1",
+--   "MakeUp_CamoEyes2",
+--   "MakeUp_CamoStripes",
+--   "MakeUp_Football",
+-- }
+
+-- local listMakeupFullFace = {
+--   "MakeUp_CamoFullFace1",
+--   "MakeUp_CamoFullFace2",
+--   "MakeUp_BraveHeart",
+--   "MakeUp_ClownFace1",
+--   "MakeUp_ClownFace2",
+--   "MakeUp_SkullFace1",
+--   "MakeUp_SkullFace2",
+-- }
+
+-- ClothingSelectionDefinitions.default.Male.listMakeupLips = {chance = 0, items = listMakeupLips}
+-- ClothingSelectionDefinitions.default.Male.listMakeupEyesShadow = {chance = 0, items = listMakeupEyesShadow}
+-- ClothingSelectionDefinitions.default.Male.listMakeupEyes = {chance = 0, items = listMakeupEyes}
+-- ClothingSelectionDefinitions.default.Male.listMakeupFullFace = {chance = 0, items = listMakeupFullFace}
+-- ClothingSelectionDefinitions.default.Female.listMakeupLips = {chance= 0, items = listMakeupLips}
+-- ClothingSelectionDefinitions.default.Female.listMakeupEyesShadow = {chance= 0, items = listMakeupEyesShadow}
+-- ClothingSelectionDefinitions.default.Female.listMakeupEyes = {chance= 0, items = listMakeupEyes}
+-- ClothingSelectionDefinitions.default.Female.listMakeupFullFace = {chance= 0, items = listMakeupFullFace}
